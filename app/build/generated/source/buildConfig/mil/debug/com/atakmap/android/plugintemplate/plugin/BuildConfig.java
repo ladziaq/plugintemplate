25a1b5c637bd906d725be1459148089d77ea38fb
marcin.ladziak@student.wat.edu.pl <SoundBox1>
2022-09-13 12:33:54 +0200

    new

--- app/build/generated/source/buildConfig/mil/debug/com/atakmap/android/plugintemplate/plugin/BuildConfig.java
+++ app/build/generated/source/buildConfig/mil/debug/com/atakmap/android/plugintemplate/plugin/BuildConfig.java
@@ -5,11 +5,11 @@ package com.atakmap.android.plugintemplate.plugin;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.atakmap.android.plugintemplate.plugin";
+  public static final String APPLICATION_ID = "com.atakmap.android.plugintemplate";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "mil";
-  public static final int VERSION_CODE = 1663051695;
-  public static final String VERSION_NAME = "4.5.1 (86ebe720) - [4.5.1]";
+  public static final int VERSION_CODE = 1663056456;
+  public static final String VERSION_NAME = "4.5.1 (526c6d39) - [4.5.1]";
   // Field from product flavor: mil
   public static final String ATAK_PACKAGE_NAME = "com.atakmap.app.civ";
 }
